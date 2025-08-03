15597755e0eac0be341b59aad0062dabbc04a873
oldkaseb <hamedsocialworks@gmail.com>
2025-08-03 22:01:08 +0400

    Create loader.py

---
+++ loader.py
@@ -0,0 +1,8 @@
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from config import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
